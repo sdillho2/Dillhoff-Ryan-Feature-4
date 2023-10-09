1070bb226169c8712dc6215f6e9d0d6d5d72a40f
Sofia Dillhoff <97558551+sdillho2@users.noreply.github.com>
2023-10-09 15:54:12 -0400

    only need to parse keys once

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import Parse from "parse";
 // Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
 // Parse.serverURL = Env.SERVER_URL;
 
-// Sofia and Celina Keys from our back4app
-Parse.serverURL = "https://parseapi.back4app.com"; // This is your Server URL
-// Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
-Parse.initialize(
-  "0B4AqrDw2p74aRMMK1TqsomsthhJHJCRO6PHfzh7", // This is your Application ID
-  "gtXFOmtfvXJpE4BBqusRIF8T1pxEHy6wogdzyEmx" // This is your Javascript key
-);
-
 function App() {
   return <Components />;
 }
