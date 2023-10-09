8b3121a18e74916f0d886344ba5afec8e1630bd8
Sofia Dillhoff <97558551+sdillho2@users.noreply.github.com>
2023-10-09 15:55:55 -0400

    removing parse from app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Components from "./Components/Components.js";
 import * as Env from "./environments";
-import Parse from "parse";
 
 // Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
 // Parse.serverURL = Env.SERVER_URL;
+// I parse in enviroments.js
 
 function App() {
   return <Components />;
