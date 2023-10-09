59ab46db1fff67e91b360ffcf121e90b9f61e345
Sofia Dillhoff <97558551+sdillho2@users.noreply.github.com>
2023-10-09 13:00:41 -0400

    deleteing the files

--- dillhoff-ryan-feature-4-kickoff-solution/src/Components/YourMatches/YourMatches.js
+++
@@ -1,8 +0,0 @@
-export default function YourMatches() {
-  return (
-    <section>
-      <h1> Welcome to YourMatches Components </h1>
-      <p> This is the YourMatches component </p>
-    </section>
-  );
-}
--- dillhoff-ryan-feature-4-kickoff-solution/src/environments.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  APPLICATION_ID: "LG9Z5QPaxemHcp1R5ENzej17Dt4FVFmnZaGhdJy5",
-  JAVASCRIPT_KEY: "LF6yM7u7jES772l9wLgpRacpjng5H1WuvQIpcDEs",
-  SERVER_URL: "https://parseapi.back4app.com/"
-};
